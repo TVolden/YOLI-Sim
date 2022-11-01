18e20ad30b6dc820fe0dec9fc1f608d66384cd6c
Thomas Volden <tv@chargetime.eu>
2022-11-01 16:36:43 +0100

    Implemented a match game

--- gym_yoli/envs/yoli_game.py
+++ gym_yoli/envs/yoli_game.py
@@ -1,76 +1,56 @@
-import gym
-from gym import spaces
+import gymnasium as gym
+from gymnasium import spaces
 import pygame
 import numpy as np
 
 class YoliGameEnv(gym.Env):
     metadata = {"render_modes": ["human", "rgb_array"], "render_fps": 4}
 
-    def __init__(self, render_mode=None, size=5):
-        self.size = size  # The size of the square grid
+    def __init__(self, render_mode=None, size=5, tiles=30):
+        self.size = size
         self.window_size = 512  # The size of the PyGame window
 
         self.observation_space = spaces.Dict(
             {
-                "position": spaces.Box(0, size - 1, shape=(2,), dtype=int),
+                "positions": spaces.Tuple((spaces.Discrete(size), spaces.Discrete(tiles+1)))
             }
         )
 
         self.action_space = spaces.Dict(
             {
-                "position": spaces.Discrete(5),
-                
+                "position": spaces.Discrete(size),
+                "tile": spaces.Discrete(tiles+1)
             }
         )
 
-        """
-        The following dictionary maps abstract actions from `self.action_space` to 
-        the direction we will walk in if that action is taken.
-        I.e. 0 corresponds to "right", 1 to "up" etc.
-        """
-        self._action_to_direction = {
-            0: np.array([1, 0]),
-            1: np.array([0, 1]),
-            2: np.array([-1, 0]),
-            3: np.array([0, -1]),
+        self._action_tiles = {
+            0: None,
+            1: {"name": "Monkey", "group": 1, "order": 1},
+            2: {"name": "Banana", "group": 1, "order": 2},
+            3: {"name": "Lion", "group": 2, "order": 1},
+            4: {"name": "Zebra", "group": 2, "order": 2}
         }
 
         assert render_mode is None or render_mode in self.metadata["render_modes"]
         self.render_mode = render_mode
 
-        """
-        If human-rendering is used, `self.window` will be a reference
-        to the window that we draw to. `self.clock` will be a clock that is used
-        to ensure that the environment is rendered at the correct framerate in
-        human-mode. They will remain `None` until human-mode is used for the
-        first time.
-        """
         self.window = None
         self.clock = None
 
     def _get_obs(self):
-        return {"agent": self._agent_location, "target": self._target_location}
+        return {"positions": tuple(self._positions)}
 
     def _get_info(self):
         return {
-            "distance": np.linalg.norm(
-                self._agent_location - self._target_location, ord=1
-            )
+            "notification": self._notification, #global notification, 0=nothing, 1=success, 2=failure
+            "indications": tuple(self._indications) #0=nothing, 1=indicated, 2=rejected
         }
 
     def reset(self, seed=None, options=None):
-        # We need the following line to seed self.np_random
         super().reset(seed=seed)
 
-        # Choose the agent's location uniformly at random
-        self._agent_location = self.np_random.integers(0, self.size, size=2, dtype=int)
-
-        # We will sample the target's location randomly until it does not coincide with the agent's location
-        self._target_location = self._agent_location
-        while np.array_equal(self._target_location, self._agent_location):
-            self._target_location = self.np_random.integers(
-                0, self.size, size=2, dtype=int
-            )
+        self._positions = [0] * self.size
+        self._indications = [0] * self.size
 
         observation = self._get_obs()
         info = self._get_info()
@@ -81,14 +61,18 @@ class YoliGameEnv(gym.Env):
         return observation, info
 
     def step(self, action):
-        # Map the action (element of {0,1,2,3}) to the direction we walk in
-        direction = self._action_to_direction[action]
-        # We use `np.clip` to make sure we don't leave the grid
-        self._agent_location = np.clip(
-            self._agent_location + direction, 0, self.size - 1
-        )
-        # An episode is done iff the agent has reached the target
-        terminated = np.array_equal(self._agent_location, self._target_location)
+        truncated = False
+        if action.position < 0 or action.position > self.size:
+            truncated = True
+        
+        self._notification = 0
+        self._indications = [0 in range(self.size)]
+
+        # Perform action
+        self._positions[action.position] = action.tile
+        self._match()
+
+        terminated = self._notification == 1
         reward = 1 if terminated else 0  # Binary sparse rewards
         observation = self._get_obs()
         info = self._get_info()
@@ -96,7 +80,23 @@ class YoliGameEnv(gym.Env):
         if self.render_mode == "human":
             self._render_frame()
 
-        return observation, reward, terminated, False, info
+        return observation, reward, terminated, truncated, info
+
+    def _match(self):
+        group = None
+        for i in range(self.size):
+            pos = self._positions[i]
+            tile = self._action_tiles[pos]
+            if tile is not None:
+                if group is None:
+                    group = tile.group
+                    self._indications[i] = 1 # Accept
+                elif group != tile.group: 
+                    self._positions[i] = 0
+                    self._indications[i] = 2 # Rejected
+                else:
+                    self._indications[i] = 1 # Accept
+                    self._notification = 1 # Win
 
     def render(self):
         if self.render_mode == "rgb_array":
