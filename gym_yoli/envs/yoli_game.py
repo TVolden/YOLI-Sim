9102d8bf8a0d58deb636d188b438166b37cbc3b8
Thomas Volden <thvo@itu.dk>
2022-11-16 14:43:55 +0100

    Transferred pygame visualization from test and added action mask internal method

--- gym_yoli/envs/yoli_game.py
+++ gym_yoli/envs/yoli_game.py
@@ -3,6 +3,8 @@ import gymnasium as gym
 from gymnasium import spaces
 import pygame
 import numpy as np
+import math
+from tile_sprites import Tile
 
 class YoliGameEnv(gym.Env):
     metadata = {"render_modes": ["human", "rgb_array"], "render_fps": 4}
@@ -16,13 +18,13 @@ class YoliGameEnv(gym.Env):
         self.observation_space = spaces.Box(low=0, high=1, shape=[size, tiles+1], dtype=np.bool8)
         self.action_space = spaces.Box(low=0, high=1, shape=[size, tiles+1], dtype=np.bool8)
 
-        self._action_tiles = {
-            0: None,
-            1: {"name": "Shape", "group": 1, "order": 1},
-            2: {"name": "Circle", "group": 1, "order": 2},
-            3: {"name": "Animal", "group": 2, "order": 1},
-            4: {"name": "Lion", "group": 2, "order": 2}
-        }
+        self._action_tiles = [
+            None,
+            {"name": "Shape", "group": 1, "order": 1},
+            {"name": "Circle", "group": 1, "order": 2},
+            {"name": "Animal", "group": 2, "order": 1},
+            {"name": "Lion", "group": 2, "order": 2}
+        ]
 
         assert render_mode is None or render_mode in self.metadata["render_modes"]
         self.render_mode = render_mode
@@ -34,6 +36,18 @@ class YoliGameEnv(gym.Env):
         oh = np.zeros((self.size, self.tiles + 1))
         oh[np.arange(self._positions), self._positions] = 1
         return oh
+
+    def _get_action_mask(self):
+        mask = np.zeros((self.size, self.tiles + 1))
+        
+        # Make a list of available tiles
+        available_tiles = [0]+[0 if tile in self._positions else 1 for tile in range(self.tiles)]
+
+        for i in range(self.size):
+            if self._positions[i]==0:
+                mask[i] = available_tiles # Must place a tile which is not already used
+            else:
+                mask[i, 0] = 1 # Only allow removal of a tile
 
     def _get_info(self):
         return {
@@ -107,49 +121,79 @@ class YoliGameEnv(gym.Env):
             self.window = pygame.display.set_mode((self.window_size, self.window_size))
         if self.clock is None and self.render_mode == "human":
             self.clock = pygame.time.Clock()
-
+        
         canvas = pygame.Surface((self.window_size, self.window_size))
         canvas.fill((255, 255, 255))
-        pix_square_size = (
-            self.window_size / (self.size + self.tiles)
-        )  # The size of a single grid square in pixels
+        
+        # Calculate unit lenghts
+        board_pix_square_size = (
+            self.window_size / self.size
+        )  # The size of a single board grid square in pixels
 
-        # Draw board
-        for x in range(self.size + 1):
+        tile_squares = math.floor(math.sqrt(self.tiles) + 1)
+        tile_pix_square_size = (
+            (self.window_size - board_pix_square_size)  / tile_squares
+        )  # The size of a single tile grid square in pixels
+        margin = 10
+        padding = 2
+
+        # Setup tile sprite
+        tile_sprites = pygame.sprite.Group()
+        
+        # Setup board grid and tile objects
+        for x in range(self.size):
+            rect = (
+                x * board_pix_square_size + margin / 2,
+                margin / 2,
+                board_pix_square_size - margin, 
+                board_pix_square_size - margin
+            )
             pygame.draw.rect(
                 canvas,
-                (255, 0, 0),
-                pygame.Rect(
-                    pix_square_size * x,
-                    (pix_square_size, pix_square_size),
-                ),
+                (0, 0, 0),
+                rect,
+                1
             )
-
-        # Finally, add some gridlines
-        for x in range(self.size + 1):
-            pygame.draw.line(
+            pos = self._positions[x]
+            if pos > 0:
+                img = self._action_tiles[pos].get("image") if len(self._action_tiles) > pos else ""
+                object_ = Tile(img, board_pix_square_size-margin-padding, board_pix_square_size-margin-padding)
+                object_.rect.x = x * board_pix_square_size + margin / 2 + padding / 2
+                object_.rect.y = margin / 2 + padding / 2
+                tile_sprites.add(object_)
+        
+        #Setup available tiles grid and tile objects
+        for x in range(self.tiles):
+            col = x % tile_squares
+            row = math.floor(x / tile_squares)
+            rect = (
+                col * tile_pix_square_size + board_pix_square_size / 2,
+                row * tile_pix_square_size + board_pix_square_size + margin / 2,
+                tile_pix_square_size - margin, 
+                tile_pix_square_size - margin
+            )
+            pygame.draw.rect(
                 canvas,
-                0,
-                (0, pix_square_size * x),
-                (self.window_size, pix_square_size * x),
-                width=3,
+                (0, 0, 0),
+                rect,
+                1
             )
-            pygame.draw.line(
-                canvas,
-                0,
-                (pix_square_size * x, 0),
-                (pix_square_size * x, self.window_size),
-                width=3,
-            )
+            pos = x+1
+            if pos not in self.positions:
+                img = self._action_tiles[pos].get("image") if len(self._action_tiles) > pos else ""
+                object_ = Tile(img, tile_pix_square_size-margin-padding, tile_pix_square_size-margin-padding)
+                object_.rect.x = col * tile_pix_square_size + board_pix_square_size / 2 + padding / 2
+                object_.rect.y = row * tile_pix_square_size + board_pix_square_size + margin / 2 + padding / 2
+                tile_sprites.add(object_)
 
         if self.render_mode == "human":
-            # The following line copies our drawings from `canvas` to the visible window
-            self.window.blit(canvas, canvas.get_rect())
+            tile_sprites.update()
+            self.window.fill((255, 255, 255))
             pygame.event.pump()
+            self.window.blit(canvas, canvas.get_rect())
+            tile_sprites.draw(self.window)
             pygame.display.update()
 
-            # We need to ensure that human-rendering occurs at the predefined framerate.
-            # The following line will automatically add a delay to keep the framerate stable.
             self.clock.tick(self.metadata["render_fps"])
         else:  # rgb_array
             return np.transpose(
