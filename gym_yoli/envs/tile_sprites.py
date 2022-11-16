19e1acd7280a298ab66789bf9f54b5e2973ab1b0
Thomas Volden <thvo@itu.dk>
2022-11-16 14:40:08 +0100

    Added guard for image files, which replaces image with a questionmark if the file does not exist

--- gym_yoli/envs/tile_sprites.py
+++ gym_yoli/envs/tile_sprites.py
@@ -1,11 +1,21 @@
 import pygame
+import os
 
 class Tile(pygame.sprite.Sprite):
 
     def __init__(self, image_file, width, height):
         pygame.sprite.Sprite.__init__(self)
 
-        self.image = pygame.image.load(image_file).convert()
-        self.image = pygame.transform.scale(self.image, (int(width), int(height)))
-
+        if (os.path.exists(image_file)):
+            self.image = pygame.image.load(image_file).convert()
+            self.image = pygame.transform.scale(self.image, (int(width), int(height)))
+        else:
+            self.image = pygame.Surface((width, height))
+            self.image.fill((255, 0, 0))
+            font = pygame.font.Font('freesansbold.ttf', 32)
+            text = font.render("?", True, (0,0,0))
+            textRect = text.get_rect()
+            textRect.center = (width//2, height//2)
+            self.image.blit(text, textRect)
+        
         self.rect = self.image.get_rect()
