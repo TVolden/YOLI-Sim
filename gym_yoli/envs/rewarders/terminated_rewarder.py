ede5400ed1d6fc16d3f1906f861d52d1c0ca4443
Thomas Volden <thvo@itu.dk>
2023-01-11 11:53:05 +0100

    Created decorator for terminated rewarder

--- gym_yoli/envs/rewarders/terminated_rewarder.py
+++ gym_yoli/envs/rewarders/terminated_rewarder.py
@@ -1,5 +1,16 @@
 from .rewarder import Rewarder
+import numpy as np
 
 class TerminatedRewarder(Rewarder):
     def reward(self, _, terminated: bool, __) -> float:
         return 1 if terminated else 0 # binary reward
+
+class TerminatedRewardDecorator(Rewarder):
+    def __init__(self, decoratee:Rewarder, value:float = 1):
+        self._decoratee = decoratee
+        self._value = value
+
+    def reward(self, indications: np.array, terminated: bool, steps: int) -> float:
+        if terminated:
+            return self._value
+        return self._decoratee.reward(indications, terminated, steps)
