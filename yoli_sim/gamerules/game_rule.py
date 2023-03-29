7214d0df6b766c7136e139d4225107ab433fee25
Thomas Volden <thvo@itu.dk>
2023-03-29 13:40:53 +0200

    Added game rule abstract class

---
+++ yoli_sim/gamerules/game_rule.py
@@ -0,0 +1,11 @@
+from abc import ABC, abstractmethod
+
+class GameRule (ABC):
+    def __init__(self, accepted_value=1, ignored_value=0, rejected_value=-1):
+        self.accepted = accepted_value
+        self.ignored = ignored_value
+        self.rejected = rejected_value
+
+    @abstractmethod
+    def evaluate(self, tiles:tuple[dict, ...]) -> tuple[int, ...]:
+        ...
--- yoli_sim/matchers/logical_operator.py
+++ yoli_sim/matchers/logical_operator.py
@@ -1,6 +1,6 @@
 class LogicalOperator:
     def __init__(self, accepted_value=1, ignored_value=0, rejected_value=-1) -> None:
-        self.accpted=accepted_value
+        self.accepted=accepted_value
         self.ignored=ignored_value
         self.rejected=rejected_value
 
