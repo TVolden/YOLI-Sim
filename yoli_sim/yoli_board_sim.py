43d0083e446eb09fa1230b9f8c4f7803daa0c54c
Thomas Volden <thvo@itu.dk>
2023-05-04 14:33:28 +0200

    Make sure to initialize terminated field

--- yoli_sim/yoli_board_sim.py
+++ yoli_sim/yoli_board_sim.py
@@ -43,6 +43,7 @@ class YoliBoardSim:
         self._positions = np.array([0] * self.size)
         self._indications = [0] * self.size
         self.notification = 0
+        self.terminated = False
         self._game.reset()
     
     def shuffle_tiles(self):
