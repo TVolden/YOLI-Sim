a09e32e094563913fc3dfd489bd8d9e088620d0a
Thomas Volden <tv@chargetime.eu>
2023-07-06 10:58:58 +0200

    Added solution check. Not sure if 0.0 is the right response.

--- yoli_sim/eval/entropy_evaluator.py
+++ yoli_sim/eval/entropy_evaluator.py
@@ -4,18 +4,32 @@ from yoli_sim.gamerules import GameRule
 class EntropyEvaluator(RuleEvaluator):
     def __init__(self, board: tuple[dict, ...], remaining: tuple[dict, ...]) -> None:
         super().__init__()
-        self.board = board
-        self.remaining = remaining
+        self._board = board
+        self._remaining = remaining
 
     def evaluate(self, rule: GameRule) -> float:
-        free_spaces = self.board.count(None)
+        free_spaces = self._board.count(None)
         if free_spaces == 0:
             return 0.0 # Already solved
         else:
-            max_entropy = len(self.remaining) * free_spaces
-            entropy_array = rule.entropy(self.board, self.remaining)
-            entropy = [tiles for pos in entropy_array for tiles in pos].count(True)
-            return 1.0 - (entropy / max_entropy)
+            entropy_per_pos = rule.entropy(self._board, self._remaining)
+            if self._has_solution(free_spaces, entropy_per_pos):
+                max_entropy = len(self._remaining) * free_spaces
+                entropy = [tiles for pos in entropy_per_pos for tiles in pos].count(True)
+                return entropy / max_entropy
+            else:
+                return 0.0 # Has no solution
+    
+    def _has_solution(self, free_spaces, entropy_per_pos) -> bool:
+        # Merge entropy using or-logic
+        entropy = []
+        for i in range(len(self._remaining)):
+            has_true = False
+            for n in range(len(entropy_per_pos)):
+                has_true = has_true or entropy_per_pos[n][i]
+            entropy.append(has_true)
+        
+        return entropy.count(True) <= free_spaces
 
 class EntropyEvaluatorFactory(RuleEvaluatorFactory):
     def create(self, _: int, board: tuple[dict, ...], remaining: tuple[dict, ...]) -> RuleEvaluator:
